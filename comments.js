50ed2aedad9ab445c9a681ab2c03e9699bad81f6
Aayushi Singh <75681857+aayushisingh11@users.noreply.github.com>
2023-11-10 08:47:25 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,17 @@
+// Create web serverconst http = require('http');
+
+// Define server hostname and port
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Create server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+// Start server
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
